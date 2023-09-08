94fac3fd5040048b47c1913c2b395f30eebd6e79
Madhuri-Sisodia <122611762+Madhuri-Sisodia@users.noreply.github.com>
2023-09-08 20:16:29 +0530

    Add files via upload

---
+++ PageDesign.js
@@ -0,0 +1,134 @@
+import React from "react";
+import "./pageDesign.css";
+
+const PageDesign = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="frame">
+            <p className="text-wrapper-4">
+              Design your dream home with Seedtree.
+            </p>
+            <p className="text-wrapper-6">
+              Custom interior design packages for single rooms or entire homes
+              with expert consultations.
+            </p>
+            <img className="ellipse" alt="Ellipse" src="./ellipse 1.png" />
+            <p className="seedtree">
+              “Seedtree made my home renovation project enjoyable and
+              stress-free. I highly recommend their interior design services to
+              anyone looking for an expert design plan.”
+              <br />
+              David Greene
+            </p>
+            <div className="overlap-group">
+              <div className="text-wrapper">Book a call</div>
+            </div>
+            <div className="overlap-2">
+              <div className="ellipse-wrapper">
+                <img className="img" alt="Ellipse" src="./ellipse 2.png" />
+              </div>
+              <p className="the-customized">
+                &#34;The customized package from Seedtree was exactly what I
+                needed to bring my vision to life. I&#39;m thrilled with my new
+                home design!
+              </p>
+              <div className="text-wrapper-2">Alessio Belcastro</div>
+            </div>
+            <div className="overlap-3">
+              <div className="div-wrapper">
+                <p className="p">
+                  Choose from a range of custom packages that fit your needs,
+                  from single room design services to full house design plans.
+                </p>
+              </div>
+              <div className="text-wrapper-3">Customized packages.</div>
+            </div>
+
+            <div className="text-wrapper-5">Expert consultations.</div>
+
+            <p className="text-wrapper-7">
+              Our experienced interior designers will work with you to create a
+              personalized design plan tailored to your unique needs and style.
+            </p>
+            <div className="text-wrapper-8">Online platform.</div>
+            <p className="text-wrapper-9">
+              Take advantage of our online platform to collaborate remotely with
+              your designer, share ideas and get real-time feedback on your
+              design project.
+            </p>
+            <div className="overlap-4">
+              <div className="rectangle" />
+              <img
+                className="screenshot"
+                alt="Screenshot"
+                src="./screenshot1 2023-04-26.png"
+              />
+            </div>
+            <div className="overlap-5">
+              <div className="rectangle-2" />
+              <img
+                className="screenshot-2"
+                alt="Screenshot"
+                src="./screenshot3 2023-04-26.png"
+              />
+            </div>
+            <div className="screenshot-wrapper">
+              <img
+                className="screenshot-3"
+                alt="Screenshot"
+                src="./screenshot2 2023-04-26.png"
+              />
+            </div>
+            <div className="rectangle-3" />
+            <div className="overlap-6">
+              <div className="text-wrapper-10">Decor package</div>
+            </div>
+            <div className="overlap-group-2">
+              <div className="text-wrapper-11">Execution package</div>
+            </div>
+            <div className="rectangle-4" />
+            <div className="overlap-7">
+              <div className="text-wrapper-12">plaining package</div>
+            </div>
+            {/* <hr></hr> */}
+            <img className="line" alt="Line" src="./line 3.svg" />
+            <img className="line-2" alt="Line" src="./line 3.svg" />
+          </div>
+          <img className="image" alt="Image" src="./image 1.png" />
+          <img className="image-2" alt="Image" src="./image 2.png" />
+          <img className="image-3" alt="Image" src="./image 3.png" />
+          <img className="image-4" alt="Image" src="./image 4.png" />
+        </div>
+        <div className="overlap-8">
+          <img
+            className="rectangle-5"
+            alt="Rectangle"
+            src="./rectangle 4.png"
+          />
+          <div className="frame-2">
+            <img className="ellipse-2" alt="Ellipse" src="./ellipse 3.png" />
+            <img className="ellipse-3" alt="Ellipse" src="./ellipse 10.png" />
+            <img className="ellipse-4" alt="Ellipse" src="./ellipse 5.png" />
+            <img className="ellipse-5" alt="Ellipse" src="./ellipse 6.png" />
+            <img className="ellipse-6" alt="Ellipse" src="./ellipse 7.png" />
+            <img className="ellipse-7" alt="Ellipse" src="./ellipse 8.png" />
+            <img className="ellipse-8" alt="Ellipse" src="./ellipse 10.png" />
+            <img className="ellipse-9" alt="Ellipse" src="./ellipse 8.png" />
+            <p className="text-wrapper-14">
+              Start designing your dream home today.
+            </p>
+            <div className="overlap-9">
+              <div className="text-wrapper-15">Book a call</div>
+            </div>
+          </div>
+        </div>
+        <div></div>
+        <img className="line-3" alt="Line" src="./line 3.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default PageDesign;
